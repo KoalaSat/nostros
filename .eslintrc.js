f705bd35705773bbbd9757336b76652fa1deb4fc
KoalaSat <yv1vtrul@duck.com>
2022-11-01 02:41:54 +0100

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
     '@typescript-eslint/no-floating-promises': 'off',
     '@typescript-eslint/no-misused-promises': 'off',
     '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/restrict-plus-operands': 'off',
   },
   settings: {
     'import/resolver': {
