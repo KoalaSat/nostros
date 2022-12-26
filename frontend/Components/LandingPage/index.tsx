588533eddbd0a8eea93b1830364b415dacfe4fa0
KoalaSat <yv1vtrul@duck.com>
2022-12-26 12:44:58 +0100

    Improve loading

--- frontend/Components/LandingPage/index.tsx
+++ frontend/Components/LandingPage/index.tsx
@@ -1,21 +1,15 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { Layout, Text, useTheme } from '@ui-kitten/components'
 import { Linking, StyleSheet, TouchableOpacity } from 'react-native'
 import Loading from '../Loading'
 import Logger from './Logger'
 import Icon from 'react-native-vector-icons/FontAwesome5'
-import { AppContext } from '../../Contexts/AppContext'
 
 export const LandingPage: React.FC = () => {
   const theme = useTheme()
-  const { goToPage } = useContext(AppContext)
 
   const onPressQuestion: () => void = () => {
     Linking.openURL('https://usenostr.org')
-  }
-
-  const onPressRelay: () => void = () => {
-    goToPage('relays')
   }
 
   const styles = StyleSheet.create({
@@ -50,9 +44,6 @@ export const LandingPage: React.FC = () => {
       <Layout style={styles.info}>
         <TouchableOpacity onPress={onPressQuestion}>
           <Icon name='question' style={styles.icon} size={24} color={theme['text-basic-color']} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={onPressRelay}>
-          <Icon name='server' style={styles.icon} size={24} color={theme['text-basic-color']} />
         </TouchableOpacity>
       </Layout>
       <Layout style={styles.tab}>
