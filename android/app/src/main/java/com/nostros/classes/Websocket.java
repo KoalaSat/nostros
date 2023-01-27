39ff0f9565afccdc3d89248a2bef989ca618484e
KoalaSat <github@koalasat.xyz>
2023-01-27 12:31:54 +0000

    Revert "New websockets library"

--- android/app/src/main/java/com/nostros/classes/Websocket.java
+++ android/app/src/main/java/com/nostros/classes/Websocket.java
@@ -6,22 +6,19 @@ import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.neovisionaries.ws.client.WebSocket;
+import com.neovisionaries.ws.client.WebSocketAdapter;
+import com.neovisionaries.ws.client.WebSocketFactory;
+import com.neovisionaries.ws.client.WebSocketFrame;
 import com.nostros.modules.DatabaseModule;
 
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.exceptions.WebsocketNotConnectedException;
-import org.java_websocket.handshake.ServerHandshake;
-
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class Websocket {
-    private WebSocketClient webSocket;
+    private WebSocket webSocket;
     private DatabaseModule database;
     private String url;
     private String pubKey;
@@ -36,58 +33,51 @@ public class Websocket {
     public void send(String message) {
         if (webSocket != null) {
             Log.d("Websocket", "SEND URL:" + url + " __ " + message);
-            try {
-                webSocket.send(message);
-            } catch (WebsocketNotConnectedException e) {
-
+            if (!webSocket.isOpen()) {
+                try {
+                    this.connect(pubKey);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
+            webSocket.sendText(message);
         }
     }
 
     public void disconnect() {
         if (webSocket != null) {
-            webSocket.close();
+            webSocket.disconnect();
         }
     }
 
-    public void connect(String userPubKey) throws IOException, URISyntaxException {
+    public void connect(String userPubKey) throws IOException {
+        WebSocketFactory factory = new WebSocketFactory();
         pubKey = userPubKey;
-        webSocket = new WebSocketClient(new URI(url)) {
-            @Override
-            public void onOpen(ServerHandshake handshakedata) {
+        webSocket = factory.createSocket(url);
+        webSocket.setMissingCloseFrameAllowed(true);
+        webSocket.setPingInterval(25 * 1000);
 
-            }
-
+        webSocket.addListener(new WebSocketAdapter() {
             @Override
-            public void onMessage(String message) {
+            public void onTextMessage(WebSocket websocket, String message) throws Exception {
                 Log.d("Websocket", "RECEIVE URL:" + url + " __ " + message);
-                JSONArray jsonArray;
-                try {
-                    jsonArray = new JSONArray(message);
-                    String messageType = jsonArray.get(0).toString();
-                    if (messageType.equals("EVENT")) {
-                        JSONObject data = jsonArray.getJSONObject(2);
-                        database.saveEvent(data, userPubKey);
-                        reactNativeEvent(data.getString("id"));
-                    } else if (messageType.equals("OK")) {
-                        reactNativeConfirmation(jsonArray.get(1).toString());
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                JSONArray jsonArray = new JSONArray(message);
+                String messageType = jsonArray.get(0).toString();
+                if (messageType.equals("EVENT")) {
+                    JSONObject data = jsonArray.getJSONObject(2);
+                    database.saveEvent(data, userPubKey);
+                    reactNativeEvent(data.getString("id"));
+                } else if (messageType.equals("OK")) {
+                    reactNativeConfirmation(jsonArray.get(1).toString());
                 }
             }
-
             @Override
-            public void onClose(int code, String reason, boolean remote) {
-                webSocket.connect();
+            public void onDisconnected(WebSocket ws, WebSocketFrame serverCloseFrame,
+                                       WebSocketFrame clientCloseFrame, boolean closedByServer) {
+                ws.connectAsynchronously();
             }
-
-            @Override
-            public void onError(Exception ex) {
-                ex.printStackTrace();
-            }
-        };
-        webSocket.connect();
+        });
+        webSocket.connectAsynchronously();
     }
 
     public void reactNativeEvent(String eventId) {
