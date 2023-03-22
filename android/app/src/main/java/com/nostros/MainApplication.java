ae44ab251fae5c29f7540f883a8e082b5388e701
KoalaSat <yv1vtrul@duck.com>
2023-03-22 16:53:01 +0100

    Working version

--- android/app/src/main/java/com/nostros/MainApplication.java
+++ android/app/src/main/java/com/nostros/MainApplication.java
@@ -24,8 +24,10 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
+
+            packages.add(new NostrosPackage());
+
+            return packages;
         }
 
         @Override
