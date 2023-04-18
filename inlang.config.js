31c441cbc0f302b5e9a42087cd91baf9afb2c778
Felix Häberle <34959078+felixhaeberle@users.noreply.github.com>
2023-04-18 14:25:06 +0000

    update inlang.config.js

--- inlang.config.js
+++ inlang.config.js
@@ -5,11 +5,13 @@
  */
 export async function defineConfig(env) {
   const plugin = await env.$import(
-    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1.1.1/dist/index.js"
+    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
   );
 
-  const { default: standardLintRules } = await env.$import(
-    "https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@1.1.1/dist/index.js"
+  const {
+    standardLintRules
+  } = await env.$import(
+    "https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@1/dist/index.js"
   );
 
   const pluginConfig = {
@@ -18,9 +20,20 @@ export async function defineConfig(env) {
 
   return {
     referenceLanguage: "en",
-    languages: await plugin.getLanguages({ ...env, pluginConfig }),
-    readResources: (args) => plugin.readResources({ ...args, ...env, pluginConfig }),
-    writeResources: (args) => plugin.writeResources({ ...args, ...env, pluginConfig }),
+    languages: await plugin.getLanguages({
+      ...env,
+      pluginConfig
+    }),
+    readResources: (args) => plugin.readResources({
+      ...args,
+      ...env,
+      pluginConfig
+    }),
+    writeResources: (args) => plugin.writeResources({
+      ...args,
+      ...env,
+      pluginConfig
+    }),
     lint: {
       rules: [standardLintRules()],
     },
